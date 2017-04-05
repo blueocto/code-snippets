b60420591564ea7b7965861786009137b2a7d915
Caroline Hagan <info@blueocto.co.uk>
2017-04-05 15:29:06 +0100

    Updated code for Featured Images

--- wordpress/featured-image-url-for-a-div.php
+++ wordpress/featured-image-url-for-a-div.php
@@ -1,2 +1,18 @@
-<?php /* Lets grab the URL for the featured image */ $featuredImage = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-<div style="background-image:url(<?php echo $featuredImage; ?>);">
+<?php 
+/**
+* It’s worth noting that has_post_thumbnail() does not just check for the Featured Image as the Codex User Contributed Note suggests. If a post contains no defined featured image but it does contain an image in the content this function will still return TRUE. so have updated this code.
+**/
+?>
+
+<?php /* Lets grab the URL for the featured image */ 
+	$featured_image_url = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+
+if  ( ! empty( $featured_image_url ) ) { ?>
+
+<div style="background-image:url(<?php echo $featured_image_url; ?>);">
+
+<?php } else { ?>
+
+<!-- do something else -->
+
+<?php } ?>
