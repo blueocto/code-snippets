291012649b35bda519173c669df17429c0c39b23
Caroline Hagan <cmurphydesign@gmail.com>
2017-04-06 10:54:41 +0100

    Removed extra character and code formatting

--- jquery-javascript/alert-window-width.js
+++ jquery-javascript/alert-window-width.js
@@ -1,27 +1,26 @@
 function checkWidth() {
-		var windowSize = $(window).width();
+	var windowSize = $(window).width();
 
-		if (windowSize < 480) {
-			alert("window width is less than 480");
+	if (windowSize < 480) {
+		alert("window width is less than 480");
+	}
+	else if (windowSize <= 768) {
+		alert("window width is less than or equal to 768");
+	}
+	else if (windowSize < 870) {
+		alert("window width is less than 870");
+	}
+	else if (windowSize <= 879) {
+		alert("window width is less than or equal to 879");
 		}
-		else if (windowSize <= 768) {
-			alert("window width is less than or equal to 768");
-		}
-		else if (windowSize < 870) {
-			alert("window width is less than 870");
-		}
-		else if (windowSize <= 879) {
-			alert("window width is less than or equal to 879");
-			}
-		else if (windowSize >= 1000) {
-			alert("window width is equal to or more than 1000");
-		}
-		else if (windowSize <= 1024) {
-			alert("window width is less than or equal to 1024");
-		}
-		else if (windowSize > 960) {
-			alert("window width is greater than 960");
-		}
+	else if (windowSize >= 1000) {
+		alert("window width is equal to or more than 1000");
+	}
+	else if (windowSize <= 1024) {
+		alert("window width is less than or equal to 1024");
+	}
+	else if (windowSize > 960) {
+		alert("window width is greater than 960");
 	}
 
 	// Execute on load
@@ -29,4 +28,4 @@ function checkWidth() {
 	// Bind event listener
 	$(window).resize(checkWidth);
 
-});
+};
