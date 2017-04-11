161e03cb44045862002448445365ccd09d84f6fe
Caroline Hagan <cmurphydesign@gmail.com>
2017-04-11 11:00:20 +0100

    Quick include of PHP snippet for WP

---
+++ wordpress/include-php-snippet-from-a-folder.php
@@ -0,0 +1,4 @@
+<!-- Include a PHP snippet .. put your php file inside  a /parts/ folder
+ so this example the file is content-features.php  -->
+
+<?php get_template_part( 'parts/content', 'features' ); ?>
