8033a1ab5f59dd211209c21f41720e831d02a2c0
Caroline Hagan <cmurphydesign@gmail.com>
2018-07-06 13:50:24 +0100

    Updated WC filter syntax for changing number of products display per page

--- woocommerce/choose-how-many-products-list-on-shop-page.php
+++ woocommerce/choose-how-many-products-list-on-shop-page.php
@@ -2,6 +2,16 @@
 
 /*** functions.php ***/
 
-add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 15;' ), 20 );
+
+/**
+* Change number of products that are displayed per page (shop page)
+*/
+function new_loop_shop_per_page( $cols ) {
+	// $cols contains the current number of products per page based on the value stored on Options -> Reading
+	// Return the number of products you wanna show per page.
+	$cols = -1; // -1 for all of them
+	return $cols;
+}
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 
 ?>
