cc396e1d4d245b9c61b02cad864a4e02aa8c8ee8
Caroline Hagan <hello@blueocto.co.uk>
2018-12-18 17:28:13 +0000

    Updates to PHP

--- wordpress/list-children-of-current-page-and-grandchildren.php
+++ wordpress/list-children-of-current-page-and-grandchildren.php
@@ -1,40 +1,51 @@
-<!-- START LIST CHILDREN OF CURRENT PAGE -->
 <?php
-	$children = get_pages( array('child_of' => $post->ID) ); 
-	if( count( $children ) != 0 ) { 
-?>
+	/*** START LIST CHILDREN OF CURRENT PAGE ***/
 
+	$children = get_pages( array('child_of' => $post->ID) );
 
-	<?php 
-		$args = array( 'post_parent' => $post->ID, 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'name', 'posts_per_page' => -1); 
-		$child_query = new WP_Query( $args ); 
-		while ( $child_query->have_posts() ) : $child_query->the_post(); 
+	//echo "This page ID: " . $children;
+	
+	if( count( $children ) != 0 ) {
+		$args = array( 'post_parent' => $post->ID, 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'name', 'posts_per_page' => -1);
+		$child_query = new WP_Query( $args );
+		while ( $child_query->have_posts() ) {
+			$child_query->the_post();
 	?>
 
-	<h2><?php the_title(); ?></h2>
-
-		<!-- START LIST GRANDCHILDREN OF LOOPING CHILDREN -->
+		<h2><?php the_title(); ?></h2>
 
 		<?php
-			$thispage = $post->ID;
-			$children = get_pages( array('child_of' => $thispage) ); 
-			if( count( $children ) != 0 ) { 
+			/*** START LIST GRANDCHILDREN OF LOOPING CHILDREN ***/
+
+			$thischild = $post->ID;
+
+			//echo "This page ID: " . $thischild;
+
+			$grandchildren = get_pages( array('child_of' => $thischild) );
+			
+			if( count( $grandchildren ) != 0 ) {
+				$gc_args = array( 
+					'post_parent' => $thischild, 
+					'post_type' => 'page', 
+					'order' => 'ASC', 
+					'orderby' => 'name', 
+					'posts_per_page' => -1
+				);
+				$grandchild_query = new WP_Query( $gc_args );
+
+				while ( $grandchild_query->have_posts() ) {
+
+					$grandchild_query->the_post(); ?>
+
+					<p><?php the_title(); ?></p>
+
+				<?php
+				} // endhwile
+			} // endif
 		?>
-		<?php 
-			$args = array( 'post_parent' => $thispage, 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'name', 'posts_per_page' => -1); 
-			$grandchild = new WP_Query( $args ); 
-			while ( $grandchild->have_posts() ) : $grandchild->the_post(); 
-		?>
 
-		<p><?php the_title(); ?></p>
-
-		<?php endwhile; wp_reset_postdata(); ?>
-		<? } ?>
-
-	<!-- // END LIST GRANDCHILDREN OF LOOPING CHILDREN -->
-
-	<?php endwhile; wp_reset_postdata(); ?>
-
-
-<?php } ?>
-<!-- // END LIST CHILDREN OF CURRENT PAGE -->
+	<?php 
+		  } // endwhile
+		  wp_reset_postdata();
+	} 
+?>
