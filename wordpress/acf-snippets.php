5bae1af6f8f9d01b9c3a7a71a987c98e89b2a1d8
Caroline Hagan <caroline@blueocto.co.uk>
2023-05-18 15:55:23 +0100

    Updated code format

--- wordpress/acf-snippets.php
+++ wordpress/acf-snippets.php
@@ -46,10 +46,8 @@
 <?php
 	// check if the repeater field has rows of data
 	if( have_rows( 'repeater_field_name' ) ):
-?>
-<?php
-	// loop through the rows of data
-	while ( have_rows( 'repeater_field_name' ) ) : the_row();
+		// loop through the rows of data
+		while ( have_rows( 'repeater_field_name' ) ) : the_row();
 ?>
 
 <?php the_sub_field( 'sub_field_name' ); ?>
