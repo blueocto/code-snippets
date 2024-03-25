dff23151077073b547a6de8f66046d386fa38df6
Caroline Hagan <hello@carolinehagan.co.uk>
2024-03-25 10:53:41 +0000

    Code tidy

--- wordpress/loop-child-pages-of-parent-page.php
+++ wordpress/loop-child-pages-of-parent-page.php
@@ -14,13 +14,15 @@
 		'name', 
 		'posts_per_page' => -1); 
 	$child_query = new WP_Query( $args ); 
-	while ( $child_query->have_posts() ) : $child_query->the_post();
+		while ( $child_query->have_posts() ) : $child_query->the_post();
 ?>
 
 <h4><?php the_title(); ?></h4>
 <p><?php $content = get_the_content(); $trimmed_content = wp_trim_words( $content, 20, '&hellip;' ); echo $trimmed_content; ?></p>
 <p><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">Read more</a></p>
 
-<?php endwhile; wp_reset_postdata(); ?>
-
-<?php } else { /*do nothing*/ } ?>
+<?php 
+		endwhile; 
+	wp_reset_postdata();
+	} 
+?>
